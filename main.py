239fa67306c7bc4d4ff0229e3d2d7e3373202ed1
bruno9812 <112516379+bruno9812@users.noreply.github.com>
2022-09-11 02:37:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ bytes=random._urandom(1024) #Creates packet to use
 ip=str(input('Target IP: ')) #The IP we are attacking locally
 port=int(input('Port: ')) #Port we direct to attack will add 1 each time it loops
 sent = 0 #Defining how many have been sent
-while 1: #Infinitely loops sending packets to the port until the program is exitede
+while 1: #Infinitely loops sending packets to the port until the program is exited.
 			sent= sent + 1 #adding one to the sent number
 			port= port + 1 #adding one to the port numbe
-			print ("Sent %s amount of packets to %s at port 80." % (sent,ip,port)) #printing how many times it has sent it and what port it sent to.
+			print ("Sent %s amount of packets to %s at port %s." % (sent,ip,port)) #printing how many times it has sent it and what port it sent to.
 			if port > 65000:
 				port = 1
